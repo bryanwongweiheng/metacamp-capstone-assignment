7183abe0b83fa99b33dec8c6cc9457bdeeba5e33
bryanwongweiheng <bryanwongweiheng@gmail.com>
2023-09-16 10:37:21 +0800

    create Country enum

---
+++ src/location.rs
@@ -0,0 +1,16 @@
+enum Country {
+    UnitedStates,
+    Canada,
+    UnitedKingdom,
+    Germany,
+    France,
+    Japan,
+    Australia,
+    China,
+    Brazil,
+    SouthKorea,
+    Ireland,
+    Spain,
+    India,
+    Switzerland
+}
